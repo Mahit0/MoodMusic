151b84f23c6f456525bd56b0a30faa259111f434
Matt le BG du 44 <you@example.com>
2026-08-07 12:18:36 +0100

    rebascul html

--- index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config(); // ou import 'dotenv/config' en ES module
-
-const clientId = process.env.SPOTIFY_CLIENT_ID;
-const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
-
-console.log("Client ID chargé :", clientId);
-
